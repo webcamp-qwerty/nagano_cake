bee0cfe2c6daf9798e90c7452783fa58855eb375
koushi6345 <k0929s0313@gmail.com>
2021-07-16 09:21:05 +0000

    admin/item finish

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,43 @@
 class Admin::ItemsController < ApplicationController
+
+  def index
+    @items = Item.page(params[:page])
+  end
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+
+  end
+
+  def create
+    @genres = Genre.all
+    @item = Item.new(item_params)
+    @item.genre_id = params[:genre][:name]
+    @item.save
+    redirect_to admin_item_path(@item)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @genres = Genre.all
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.genre_id = params[:genre][:name]
+    item.update(item_params)
+    redirect_to admin_item_path(item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduct, :genre_id, :excluded_price, :is_active)
+  end
+
 end
