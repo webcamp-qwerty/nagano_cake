a97fb580a1889f86fe363bd122f9fb461272a467
koushi6345 <k0929s0313@gmail.com>
2021-07-17 05:23:38 +0000

    public/item finish

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,13 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+    @items = Item.page(params[:page]).per(8)
+    @genres = Genre.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
 end
