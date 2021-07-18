d52c750cb5352647e408bbacfb93ac6c1fa89b6f
sgmt1001 <sgmt1001@gmail.com>
2021-07-18 09:22:05 +0000

    near_completed

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,7 +17,6 @@ class Admin::CustomersController < ApplicationController
         @customer = Customer.find(params[:id])
         if  @customer.update(customer_params)
             redirect_to admin_customer_path(@customer), notice: "会員情報を更新しました"
-            #リダイレクト先のURLが「/admin/customer/:id」にならずに「/customers」（publicのcustomersのshow）に飛ぶ。要確認
         else
             render "edit", notice: "会員情報を更新できませんでした"
         end
