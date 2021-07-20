1457a50275712a7b8bdff8f542def8d7433f73c9
EC2 Default User <ec2-user@ip-172-31-37-158.ec2.internal>
2021-07-20 08:21:53 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,8 +8,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.new  
-    # @cart_items = current_customer.cart_items
+    @order = Order.new
+    @cart_items = current_customer.cart_items
+    @order.delivery_charge = 800
+    @order.customer_id = current_customer.id
+
     @order.payment_method = params[:order][:payment_method]
     if params[:order][:address_option] == "0"
       @order.postcode = current_customer.postcode
@@ -25,21 +28,47 @@ class Public::OrdersController < ApplicationController
       @order.postcode = params[:order][:postcode]
       @order.address = params[:order][:address]
       @order.name = params[:order][:name]
-      
+
     end
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    @cart_items = current_customer.cart_items
+    
+   @cart_items.each do |cart_item| 
+    @order_item = OrderItem.new
+    @order_item.order_id = @order.id
+    @order_item.item_id = cart_item.item_id
+    @order_item.item_count = cart_item.item_count
+    @order_item.purchase_price = cart_item.item.add_tax_price
+    @order_item.save
+   end 
+    current_customer.cart_items.destroy_all
+    
+    redirect_to order_thanks_path
   end
+
+
 
   def thanks
   end
 
   def index
+    @orders = Order.page(params[:order]).per(8)
   end
 
   def show
   end
+
+ private
+  def order_params
+    params.require(:order).permit(:customer_id, :postcode, :address, :name, :payment, :delivery_charge, :payment_method, :order_status)
+  end
+  # def order_item_params
+  #   params.require(:order_item).permit(:item_id, :item_count, :purchase_price, :order_id, :make_status)
+  # end
 
 
 end
