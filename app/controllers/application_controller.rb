017dd4f9e2a55f1087705adcae42ff59ef62a7e3
sgmt1001 <sgmt1001@gmail.com>
2021-07-22 07:57:01 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_customer!,except: [:top, :about], if: :cus_auth?
+  before_action :authenticate_admin!
 
+private
+  def cus_auth?
+    unless controller_name == 'items' && action_name == 'index'
+      true
+    end
+  end
 
-  private
+protected
+  def after_sign_in_path_for(resource)
+    if current_admin
+      orders_path
+    else
+      root_path
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number])
