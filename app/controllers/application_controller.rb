c2b0a1eedac4ecf3bf56dc70ced4a65e70e2a294
koushi6345 <k0929s0313@gmail.com>
2021-07-24 04:54:56 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 protected
   def after_sign_in_path_for(resource)
     if current_admin
-      orders_path
+      admin_orders_path
     else
       root_path
     end
