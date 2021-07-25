03f6e328977d967bdcd1c416c1691b5b493b3bc6
sgmt1001 <sgmt1001@gmail.com>
2021-07-25 10:39:40 +0000

    update

--- app/controllers/admin/searchs_controller.rb
+++ app/controllers/admin/searchs_controller.rb
@@ -1,5 +1,43 @@
 class Admin::SearchsController < Admin::ApplicationController
-  
+
   def search
+    @value = params["search"]["value"]
+    @how = params["search"]["how"]
+    @name = params["search"]["name"]
+    @datas = search_for(@how, @value)
   end
+
+private
+
+  def match(value)
+      Customer.where_sql(last_name: value)
+      #concat?
+      #||で区切る
+  end
+
+  def forward(value)
+      Customer.where("name LIKE ?", "#{value}%")
+  end
+
+  def backward(value)
+      Customer.where("name LIKE ?", "%#{value}")
+  end
+
+  def partical(value)
+      Customer.where("name LIKE ?", "%#{value}%")
+  end
+
+  def search_for(how, value)
+    case how
+    when 'match'
+      match(value)
+    when 'forward'
+      forward(value)
+    when 'backward'
+      backward(value)
+    when 'partical'
+      partical(value)
+    end
+  end
+
 end
